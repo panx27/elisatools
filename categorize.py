6d2870f3ffa0b1a703331d0092f82e199f144961
Jonathan May <jonmay@isi.edu>
2015-10-02 11:10:24 -0700

    fixed categorize bugs and verified correct behavior

--- categorize.py
+++ categorize.py
@@ -11,18 +11,19 @@ scriptdir = os.path.dirname(os.path.abspath(__file__))
 # http://stackoverflow.com/questions/600268/mkdir-p-functionality-in-python
 import os, errno
 def mkdir_p(path):
-    try:
-        os.makedirs(path)
-    except OSError as exc: # Python >2.5
-        if exc.errno == errno.EEXIST and os.path.isdir(path):
-            pass
-        else: raise
+  try:
+    os.makedirs(path)
+  except OSError as exc: # Python >2.5
+    if exc.errno == errno.EEXIST and os.path.isdir(path):
+      pass
+    else: raise
 
 def main():
-  parser = argparse.ArgumentParser(description="Given category per doc and doc file, put data in category-specific dir",
+  parser = argparse.ArgumentParser(description="Given category per doc, idfile, data file, put data in category-specific dir",
                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
   parser.add_argument("--catfile", "-c", nargs='?', type=argparse.FileType('r'), help="doc cat file (docid cat)")
-  parser.add_argument("--infile", "-i", nargs='?', type=argparse.FileType('r'), default=sys.stdin, help="input file (docid data)")
+  parser.add_argument("--idfile", "-d", nargs='?', type=argparse.FileType('r'), help="id file (docid per line)")
+  parser.add_argument("--infile", "-i", nargs='?', type=argparse.FileType('r'), default=sys.stdin, help="input file")
   parser.add_argument("--prefix", "-p", default=".", help="directory prefix for categories")
 
   try:
@@ -34,18 +35,22 @@ def main():
   writer = codecs.getwriter('utf8')
   catfile = reader(args.catfile)
   infile = reader(args.infile)
+  idfile = reader(args.idfile)
 
+  basefile = os.path.basename(args.infile.name)
   cats = {}
-  basefile = os.path.basename(args.infile)
+  fhs = {}
   for line in catfile:
     doc, cat = line.strip().split('\t')
     prefix = os.path.join(args.prefix, cat)
-    mkdir_p(prefix)
-    cats[doc]=writer(open(os.path.join(prefix, basefile), 'w'))
+    catfile = os.path.join(prefix, basefile)
+    if catfile not in fhs:      
+      mkdir_p(prefix)
+      fhs[catfile]=writer(open(catfile, 'w'))
+    cats[doc]=fhs[catfile]
 
-  for line in infile:
-    doc, data = line.strip().split('\t')
-    cats[doc].write(data+"\n")
+  for lid, (doc, data) in enumerate(izip(idfile, infile)):
+    cats[doc.strip()].write(data)
 
 if __name__ == '__main__':
   main()
