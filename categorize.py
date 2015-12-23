abf8aec957be061f4020185c213c6d3827be002f
Jon May <jonmay@isi.edu>
2015-12-22 16:44:15 -0800

    reselect, changes to categorize

--- categorize.py
+++ categorize.py
@@ -17,6 +17,7 @@ def main():
   parser.add_argument("--infile", "-i", nargs='?', type=argparse.FileType('r'), default=sys.stdin, help="input file")
   parser.add_argument("--prefix", "-p", default=".", help="directory prefix for categories")
   parser.add_argument("--postfix", "-P", default=".", help="directory postfix after categories")
+  parser.add_argument("--remainder", "-r", default="train", help="remainder category. Should match previous remainder category")
 
   try:
     args = parser.parse_args()
@@ -35,14 +36,21 @@ def main():
   for line in catfile:
     doc, cat = line.strip().split('\t')
     prefix = os.path.join(args.prefix, cat, args.postfix)
-    catfile = os.path.join(prefix, basefile)
-    if catfile not in fhs:      
+    innercatfile = os.path.join(prefix, basefile)
+    if innercatfile not in fhs:      
       mkdir_p(prefix)
-      fhs[catfile]=writer(open(catfile, 'w'))
-    cats[doc]=fhs[catfile]
-
+      fhs[innercatfile]=writer(open(innercatfile, 'w'))
+    cats[doc]=fhs[innercatfile]
+  remcatpref = os.path.join(args.prefix, args.remainder, args.postfix)
+  remaindercatfile = os.path.join(remcatpref, basefile)
+  if remaindercatfile not in fhs:
+    mkdir_p(remcatpref)
+    fhs[remaindercatfile]=writer(open(remaindercatfile, 'w'))
+    
   for doc, data in izip(idfile, infile):
-    cats[doc.strip()].write(data)
+    doc = doc.strip()
+    fh = cats[doc] if doc in cats else fhs[remaindercatfile]
+    fh.write(data)
 
 if __name__ == '__main__':
   main()
--- reselect_data.py
+++ reselect_data.py
@@ -10,41 +10,28 @@ from lputil import mkdir_p
 from subprocess import check_output, STDOUT, CalledProcessError
 scriptdir = os.path.dirname(os.path.abspath(__file__))
 
-
-def runselection(prefix, idfile, engfile, termfile, categories, remainder, sizes, filetypes, srclang, indir, outdir):
-  ''' do a data selection and apply it to a set of files '''
-  rankfile = os.path.join(outdir, "%s.ranks" % prefix)
-  countfile = os.path.join(outdir, "%s.counts" % prefix)
-  catfile = os.path.join(outdir, "%s.cats" % prefix)
+def runselection(prefix, idfile, catfile, remainder, filetypes, srclang, indir, outdir):
+  ''' apply a previous data selection to a set of files '''
   try:
-    cmd_output=check_output("%s/rankdocuments.py -i %s -d %s -t %s -o %s" % 
-                            (scriptdir, engfile, idfile, termfile, rankfile), stderr=STDOUT, shell=True)
-    cmd_output=check_output("%s/countwords.py -i %s -d %s -o %s" % 
-                            (scriptdir, engfile, idfile, countfile), stderr=STDOUT, shell=True)
-    cmd_output=check_output("%s/roundrobin.py -w %s -f %s -s %s -c %s -r %s -o %s" % 
-                            (scriptdir, countfile, rankfile, sizes, categories, remainder, catfile), stderr=STDOUT, shell=True)
     for filetype in filetypes:
       if os.path.exists(os.path.join(indir, filetype)):
         for flang in [srclang, 'eng']:
           flatfile = os.path.join(indir, filetype, "%s.%s.%s.flat" % (prefix, filetype, flang))
-          cmd = "%s/categorize.py -i %s -d %s -c %s -p %s -P %s" % (scriptdir, flatfile, idfile, catfile, outdir, filetype)
+          cmd = "%s/categorize.py -i %s -d %s -c %s -p %s -P %s -r %s" % (scriptdir, flatfile, idfile, catfile, outdir, filetype, remainder)
           print "Running "+cmd
           cmd_output=check_output(cmd, stderr=STDOUT, shell=True)
   except CalledProcessError as exc:
     print "Status : FAIL", exc.returncode, exc.output
     sys.exit(1)
-  return catfile
+
 
 def main():
-  parser = argparse.ArgumentParser(description="Make dataset selections for experimentation",
+  parser = argparse.ArgumentParser(description="Make dataset selections for experimentation given previously generated categorization files",
                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
   parser.add_argument("--indir", "-i", help="location of parallel data")
   parser.add_argument("--language", "-l", help="source language three digit code")
-  parser.add_argument("--minimum", "-m", default=100, help="minimum number of words per subselection")
-  parser.add_argument("--sizes", "-s", nargs='+', type=int, help="list of sizes desired in each category")
-  parser.add_argument("--categories", "-c", nargs='+', help="list of categories. Must match sizes")
-  parser.add_argument("--termfile", "-t", help="file of desired terms, one per line")
-  parser.add_argument("--remainder", "-r", default="train", help="remainder category. Should be a new category")
+  parser.add_argument("--remainder", "-r", default="train", help="remainder category. Should match previous remainder category")
+  parser.add_argument("--previous", "-p", help="location of previous cat files")
 
 
 
@@ -58,8 +45,6 @@ def main():
 #  outfile = writer(args.outfile)
 
   indir = args.indir
-  origsizes = args.sizes
-  termfile = args.termfile
   # TODO: find these?
   # doc = keep full docs together  (can detect this by counting number of unique docs)
   # TODO: re-add found.generic to docprefixes
@@ -74,10 +59,6 @@ def main():
   outpath = os.path.join(indir, 'splits')
   mkdir_p(outpath)
 
-  # number of words in each file
-  fullsizes = {}
-  adjsizes = {}
-  sizesum = 0.0
   for preflist in [docprefixes, nodocprefixes]:
     for prefix in list(preflist):
       # don't deal with it more if there's nothing in the manifest
@@ -85,18 +66,7 @@ def main():
       if (not os.path.exists(manfile)) or os.path.getsize(manfile) == 0:
         print "removing "+prefix
         preflist.remove(prefix)
-  for prefix in docprefixes+nodocprefixes:      
-    engfile=os.path.join(origpath, "%s.original.eng.flat" % prefix)
-    prefsize = int(check_output("wc -w %s" % engfile, shell=True).strip().split(' ')[0])
-    fullsizes[prefix] = prefsize
-    sizesum +=prefsize
-  # adjust size split by proportion, with minimum
-  for prefix in docprefixes+nodocprefixes:
-    mult = fullsizes[prefix]/sizesum
-    adjsizes[prefix] = map(lambda x: max(args.minimum, int(mult*x)), origsizes)
-    print prefix,adjsizes[prefix]
   # doc-based processing
-  catlist = ' '.join(args.categories)
   for prefix in docprefixes:
     idfile = os.path.join(outpath, "%s.ids" % prefix)
     manfile = os.path.join(extractpath, "%s.eng.manifest" % prefix)
@@ -104,12 +74,11 @@ def main():
       check_output("cut -f2 %s > %s" % (manfile, idfile), stderr=STDOUT, shell=True)
     except CalledProcessError as exc:
       print "Status : FAIL", exc.returncode, exc.output
-    engfile=os.path.join(origpath, "%s.original.eng.flat" % prefix)
-    sizelist = ' '.join(map(str, adjsizes[prefix])) 
-    catfile = runselection(prefix, idfile, engfile, termfile, catlist, args.remainder, sizelist, filetypes, args.language, extractpath, outpath)
+    catfile = os.path.join(args.previous, "%s.cats" % prefix)  
+    runselection(prefix, idfile, catfile, args.remainder, filetypes, args.language, extractpath, outpath)
     for i in (args.language, 'eng'):
       manifest = os.path.join(extractpath, "%s.%s.manifest" % (prefix, i))
-      cmd = "%s/categorize.py -i %s -d %s -c %s -p %s" % (scriptdir, manifest, idfile, catfile, outpath)
+      cmd = "%s/categorize.py -i %s -d %s -c %s -p %s -r %s" % (scriptdir, manifest, idfile, catfile, outpath, args.remainder)
       print "Running "+cmd
       check_output(cmd, stderr=STDOUT, shell=True)
 
@@ -122,12 +91,11 @@ def main():
       check_output("seq %d > %s" % (mansize, idfile), stderr=STDOUT, shell=True)
     except CalledProcessError as exc:
       print "Status : FAIL", exc.returncode, exc.output
-    engfile=os.path.join(origpath, "%s.original.eng.flat" % prefix)
-    sizelist = ' '.join(map(str, adjsizes[prefix])) 
-    catfile = runselection(prefix, idfile, engfile, termfile, catlist, args.remainder, sizelist, filetypes, args.language, extractpath, outpath)
+    catfile = os.path.join(args.previous, "%s.cats" % prefix)
+    runselection(prefix, idfile, catfile, args.remainder, filetypes, args.language, extractpath, outpath)
     for i in (args.language, 'eng'):
       manifest = os.path.join(extractpath, "%s.%s.manifest" % (prefix, i))
-      cmd = "%s/categorize.py -i %s -d %s -c %s -p %s" % (scriptdir, manifest, idfile, catfile, outpath)
+      cmd = "%s/categorize.py -i %s -d %s -c %s -p %s -r %s" % (scriptdir, manifest, idfile, catfile, outpath, args.remainder)
       print "Running "+cmd
       check_output(cmd, stderr=STDOUT, shell=True)
 
