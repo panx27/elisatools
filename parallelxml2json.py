1fca9f57f421104294bdc13f61cb2b28f7899897
Jon May <jonmay@isi.edu>
2016-03-21 12:05:11 -0700

    parallelxml2json for dry run

---
+++ parallelxml2json.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+import argparse
+import sys
+import codecs
+if sys.version_info[0] == 2:
+  from itertools import izip
+else:
+  izip = zip
+from collections import defaultdict as dd
+import re
+import os.path
+import gzip
+from lxml import etree as ET
+import json
+scriptdir = os.path.dirname(os.path.abspath(__file__))
+
+
+reader = codecs.getreader('utf8')
+writer = codecs.getwriter('utf8')
+
+
+def prepfile(fh, code):
+  ret = gzip.open(fh.name, code if code.endswith("t") else code+"t") if fh.name.endswith(".gz") else fh
+  if sys.version_info[0] == 2:
+    if code.startswith('r'):
+      ret = reader(fh)
+    elif code.startswith('w'):
+      ret = writer(fh)
+    else:
+      sys.stderr.write("I didn't understand code "+code+"\n")
+      sys.exit(1)
+  return ret
+
+
+
+def main():
+  parser = argparse.ArgumentParser(description="convert elisa parallel data into json format",
+                                   formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument("--infile", "-i", nargs='?', type=argparse.FileType('rb'), default=sys.stdin, help="input file")
+  parser.add_argument("--outfile", "-o", nargs='?', type=argparse.FileType('w'), default=sys.stdout, help="output file")
+
+
+  # TODO: actually implement this!
+
+  try:
+    args = parser.parse_args()
+  except IOError as msg:
+    parser.error(str(msg))
+
+  infile = prepfile(args.infile, 'r')
+  outfile = prepfile(args.outfile, 'w')
+
+
+  for line in infile:
+    outfile.write(line)
+
+if __name__ == '__main__':
+  main()
+
