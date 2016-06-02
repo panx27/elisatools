37a0028836e1c4410eedd9d2dd8a6534f5e7229b
Jon May <jonmay@isi.edu>
2016-06-01 22:36:14 -0700

    update to instructions

--- lputil.py
+++ lputil.py
@@ -363,7 +363,6 @@ def pair_found_files_from_al_xml(srcdir, trgdir, aldir):
   srcfiles = os.listdir(srcdir)
   alfiles = os.listdir(aldir)
   for alfile in alfiles:
-    
     alroot = ET.parse(os.path.join(aldir, alfile)).getroot()
     sid=alroot.get('source_id')
     tid=alroot.get('translation_id')
