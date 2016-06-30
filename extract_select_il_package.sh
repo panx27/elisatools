5436f9c64f874045fff67c5ac44809dd2e0a7470
Jon May <jonmay@isi.edu>
2016-06-30 09:18:34 -0700

    extract select for eval il

---
+++ extract_select_il_package.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -e
+SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+TARBALL=$1;
+LANGUAGE=$2;
+DST=$3;
+VERSION=$4;
+KEY=$5;
+DEVSET=$6;
+
+
+mkdir -p $DST/$LANGUAGE
+if [ -n "$KEY" ]; then
+    $SCRIPTDIR/one_button_lrlp.py --evalil -t $TARBALL -l $LANGUAGE -r $DST -k $KEY -S set0 &> $DST/$LANGUAGE/one_button_lrlp.err
+else
+    $SCRIPTDIR/one_button_lrlp.py --evalil -t $TARBALL -l $LANGUAGE -r $DST &> $DST/$LANGUAGE/one_button_lrlp.err
+fi
+if [[ -n "$DEVSET" ]] && [[ -e $DEVSET ]];  then
+    PREFIX="-d $DEVSET";
+else
+    PREFIX=""
+fi
+$SCRIPTDIR/subselect_data.py $PREFIX -i $DST/$LANGUAGE/parallel -e filtered -l $LANGUAGE -s 10000 10000 20000 -c syscomb test dev -t $SCRIPTDIR/incidentvocab &> $DST/$LANGUAGE/subselect_data.err
+$SCRIPTDIR/one_button_package.py --noeval -l $LANGUAGE -v $VERSION -r $DST/$LANGUAGE &> $DST/$LANGUAGE/one_button_package.err
+for i in syscomb test dev train rejected; do
+    $SCRIPTDIR/elisa2flat.py -f FULL_ID_SOURCE SOURCE.id ORIG_RAW_SOURCE ORIG_RAW_TARGET -i $DST/$LANGUAGE/elisa.$LANGUAGE-eng.$i.y1r1.v$VERSION.xml.gz -o $DST/$LANGUAGE/$i.tab; echo $i; $SCRIPTDIR/sample.py -i $DST/$LANGUAGE/$i.tab -s 10;
+done > $DST/$LANGUAGE/samples;
+
+$SCRIPTDIR/elisa2flat.py -f FULL_ID_SOURCE SOURCE.id ORIG_RAW_SOURCE ORIG_RAW_TARGET -i $DST/$LANGUAGE/elisa.$LANGUAGE.y1r1.v$VERSION.xml.gz -o $DST/$LANGUAGE/$LANGUAGE.tab
+echo "Done with $LANGUAGE version $VERSION in $DST";
+ls -l $DST/$LANGUAGE
