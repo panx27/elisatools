32cc782ffe2743efc45c75a18fc9e8edcd6fd864
Jonathan May <jonmay@isi.edu>
2017-05-17 17:20:33 -0700

    wrapper to extract tokens from ltf

---
+++ ltf_tok.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# code by Jon May [jonmay@isi.edu]
+import argparse
+import sys
+import codecs
+if sys.version_info[0] == 2:
+  from itertools import izip
+else:
+  izip = zip
+from collections import defaultdict as dd
+import re
+import os.path
+import gzip
+import tempfile
+import shutil
+import atexit
+import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import ParseError
+
+scriptdir = os.path.dirname(os.path.abspath(__file__))
+
+reader = codecs.getreader('utf8')
+writer = codecs.getwriter('utf8')
+
+
+def prepfile(fh, code):
+  if type(fh) is str:
+    fh = open(fh, code)
+  ret = gzip.open(fh.name, code) if fh.name.endswith(".gz") else fh
+  if sys.version_info[0] == 2:
+    if code.startswith('r'):
+      ret = reader(fh)
+    elif code.startswith('w'):
+      ret = writer(fh)
+    else:
+      sys.stderr.write("I didn't understand code "+code+"\n")
+      sys.exit(1)
+  return ret
+
+def addonoffarg(parser, arg, dest=None, default=True, help="TODO"):
+  ''' add the switches --arg and --no-arg that set parser.arg to true/false, respectively'''
+  group = parser.add_mutually_exclusive_group()
+  dest = arg if dest is None else dest
+  group.add_argument('--%s' % arg, dest=dest, action='store_true', default=default, help=help)
+  group.add_argument('--no-%s' % arg, dest=dest, action='store_false', default=default, help="See --%s" % arg)
+
+def main():
+  parser = argparse.ArgumentParser(description="extract tokens from an ltf file",
+                                   formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  addonoffarg(parser, 'debug', help="debug mode", default=False)
+  parser.add_argument("--infile", "-i", nargs='?', type=argparse.FileType('r'), default=sys.stdin, help="input file")
+  parser.add_argument("--outfile", "-o", nargs='?', type=argparse.FileType('w'), default=sys.stdout, help="output file")
+
+  workdir = tempfile.mkdtemp(prefix=os.path.basename(__file__), dir=os.getenv('TMPDIR', '/tmp'))
+
+  try:
+    args = parser.parse_args()
+  except IOError as msg:
+    parser.error(str(msg))
+
+  def cleanwork():
+    shutil.rmtree(workdir, ignore_errors=True)
+  if args.debug:
+    print(workdir)
+  else:
+    atexit.register(cleanwork)
+
+  infile = prepfile(args.infile, 'r')
+  outfile = prepfile(args.outfile, 'w')
+  try:
+    xobj = ET.parse(infile)
+    for x in xobj.findall(".//SEG"):
+      tokens = x.findall(".//TOKEN")
+      toktext = []
+      for y in tokens:
+        if y.text is None:
+          continue
+        toktext.append(y.text)
+      outfile.write(' '.join(toktext)+"\n")
+  except ET.ParseError:
+    sys.stderr.write("Parse error on "+infile.name+"\n")
+if __name__ == '__main__':
+  main()
